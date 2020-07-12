64c2b5b90842174968d29c1cf750a53d6a4cb954
ashoka25395 <akgias2018@gmail.com>
2020-07-12 16:52:17 +0530

    seed data

---
+++ src/main/resources/seed_data/user.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM ratingsystem.user
+LIMIT 0, 1000
+
+-- Date: 2020-07-12 16:44
+*/
+INSERT INTO `ratingsystem.user` (`id`,`created_at`,`first_name`,`last_name`,`updated_at`,`user_type`) VALUES (1,'2020-07-12 14:00:00.000000','ashok','gupta','2020-07-12 14:00:00.000000','PASSENGER');
+INSERT INTO `ratingsystem.user` (`id`,`created_at`,`first_name`,`last_name`,`updated_at`,`user_type`) VALUES (2,'2020-07-12 14:00:00.000000','rakesh','gupta','2020-07-12 14:00:00.000000','PASSENGER');
+INSERT INTO `ratingsystem.user` (`id`,`created_at`,`first_name`,`last_name`,`updated_at`,`user_type`) VALUES (3,'2020-07-12 14:00:00.000000','aryan','gupta','2020-07-12 14:00:00.000000','DRIVER');
+INSERT INTO `ratingsystem.user` (`id`,`created_at`,`first_name`,`last_name`,`updated_at`,`user_type`) VALUES (4,'2020-07-12 14:00:00.000000','gaurav','gupta','2020-07-12 14:00:00.000000','DRIVER');
+INSERT INTO `ratingsystem.user` (`id`,`created_at`,`first_name`,`last_name`,`updated_at`,`user_type`) VALUES (5,'2020-07-12 14:00:00.000000','pankaj','gupta','2020-07-12 14:00:00.000000','PASSENGER');
---
+++ src/main/resources/seed_data/user_rating.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM ratingsystem.user_rating
+LIMIT 0, 1000
+
+-- Date: 2020-07-12 16:45
+*/
+INSERT INTO `ratingsystem.user_rating` (`id`,`created_at`,`rating`,`updated_at`,`user_id`) VALUES (1,'2020-07-12 15:13:59.183000',4,'2020-07-12 15:13:59.183000',2);
+INSERT INTO `ratingsystem.user_rating` (`id`,`created_at`,`rating`,`updated_at`,`user_id`) VALUES (2,'2020-07-12 15:14:20.301000',4,'2020-07-12 15:14:20.301000',5);
+INSERT INTO `ratingsystem.user_rating` (`id`,`created_at`,`rating`,`updated_at`,`user_id`) VALUES (3,'2020-07-12 15:14:26.005000',4,'2020-07-12 15:14:26.005000',3);
+INSERT INTO `ratingsystem.user_rating` (`id`,`created_at`,`rating`,`updated_at`,`user_id`) VALUES (4,'2020-07-12 15:14:42.234000',5,'2020-07-12 15:14:42.234000',3);
+INSERT INTO `ratingsystem.user_rating` (`id`,`created_at`,`rating`,`updated_at`,`user_id`) VALUES (5,'2020-07-12 15:14:49.194000',4,'2020-07-12 15:14:49.194000',3);
+INSERT INTO `ratingsystem.user_rating` (`id`,`created_at`,`rating`,`updated_at`,`user_id`) VALUES (6,'2020-07-12 15:14:55.073000',2,'2020-07-12 15:14:55.073000',3);
